ad2477b2458db18d5c7025b5e8efbb2c2fdb4ad6
Mevaser Zehoray <Mevaserruppin@gmail.com>
2025-03-20 09:44:49 +0200

    add random choice fix function moving car

--- car_manager.py
+++ car_manager.py
@@ -1,9 +1,10 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
-from turtle import Turtle
-import random
 
 class CarManager:
     def __init__(self):
@@ -11,20 +12,22 @@ class CarManager:
         self.car_speed = STARTING_MOVE_DISTANCE
 
     def create_car(self):
-            new_car = Turtle()
-            new_car.shape("square")
-            new_car.shapesize(stretch_wid=2, stretch_len=4)
-            new_car.penup()
-            new_car.color(random.choice(COLORS))
-            random_y = random.randint(-250, 250)
-            new_car.goto(300, random_y)
-            self.all_cars.append(new_car)
+            random_chance = random.randint(1, 6)
+            if random_chance == 1:
+                new_car = Turtle("square")
+                new_car.shapesize(stretch_wid=1, stretch_len=2)
+                new_car.penup()
+                new_car.color(random.choice(COLORS))
+                random_y = random.randint(-250, 250)
+                new_car.goto(350, random_y)
+                self.all_cars.append(new_car)
 
+    
     
 
     def move_cars(self):
         for car in self.all_cars:
-            car.goto(car.xcor() - self.car_speed, car.ycor())
+            car.backward(self.car_speed)
 
 
     def level_up(self):
