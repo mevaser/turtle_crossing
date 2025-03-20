86d02bfe07d84d4bc80d15a2a9c4ffacf048a9ef
Mevaser Zehoray <Mevaserruppin@gmail.com>
2025-03-20 09:46:45 +0200

    fix inheritance

--- player.py
+++ player.py
@@ -1,25 +1,25 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-from turtle import Turtle
-
-class Player:
+class Player(Turtle):
     def __init__(self):
-        self.turtle = Turtle()
-        self.turtle.shape("turtle")
-        self.turtle.penup()
-        self.turtle.setheading(90)
-        self.turtle.goto(STARTING_POSITION)
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
 
-    def move(self):
-        self.turtle.forward(MOVE_DISTANCE)
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
 
     def reset_position(self):
-        self.turtle.goto(STARTING_POSITION)
+        self.goto(STARTING_POSITION)
 
     def is_at_finish_line(self):
-        if self.turtle.ycor() > FINISH_LINE_Y:
+        if self.ycor() > FINISH_LINE_Y:
             return True
         else:
             return False
